496a2c5db1cace2a27fb834d0507a42de947b159
Ian D. Miller <iandouglas96@gmail.com>
2017-07-24 22:00:36 -0400

    Added gauge

--- public/client.js
+++ public/client.js
@@ -1,7 +1,31 @@
 //Connect to the websocket
 var socket = io();
 
-//Create chart object to 
+//gauge for instantaneous power
+var power_gauge = c3.generate({
+    bindto: '#power_gauge',
+    data: {
+        columns: [
+            ['power', 0]
+        ],
+        type: 'gauge'
+    },
+    gauge: {
+        label: {
+            format: function (value, ratio) {
+                return parseFloat(value).toFixed(2);
+            }
+        },
+        min: 0,
+        max: 6.2,
+        units: ' kW'
+    },
+    size: {
+        height: 150
+    }
+});
+
+//Create chart object to show power over time
 var power_chart = c3.generate({
 	bindto: '#power_chart',
 	data: {
@@ -14,13 +38,16 @@ var power_chart = c3.generate({
 //Have new data, push it to the graph
 socket.on('data', function(data) {
 	console.log(data);
+    //update graph
     power_chart.flow({
         columns: [
             ['power', data.power]
         ],
-        duration: 1500
+        duration: 1500,
+        to: 0
+    });
+    //update gauge
+    power_gauge.load({
+        columns: [['power', data.power]]
     });
 });
-
-
-
